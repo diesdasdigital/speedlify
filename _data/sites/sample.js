06e104c275348d691258f92892d2bdc328b6a20a
Maximilian Hoffmann <hey@max.hn>
2020-09-03 10:17:22 +0200

    update sites to test

--- _data/sites/sample.js
+++ _data/sites/sample.js
@@ -1,10 +1,15 @@
 module.exports = {
-	name: "Sample", // optional, falls back to object key
-	description: "The default sites that get tested",
-	options: {
-		frequency: 60 * 23, // (in minutes), 23 hours
-	},
-	urls: [
-		"https://www.speedlify.dev/"
-	]
+  name: "Live Websites", // optional, falls back to object key
+  description: "Sites that we have developed and have launched",
+  options: {
+    frequency: 60 * 23, // (in minutes), 23 hours
+  },
+  urls: [
+    "https://www.diesdas.digital/",
+    "https://www.kicommunity.de/",
+    "https://infarm.de/",
+    "https://www.psw-engineering.de/",
+    "https://3d-akademie.de/",
+    "https://www.agripolicykit.net/",
+  ],
 };
